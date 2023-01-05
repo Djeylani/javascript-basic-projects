c9bdb021a948a3d7e9652609e4bb4de7cb10ef2f
Dee Jeylani <shariifbeforem@gmail.com>
2023-01-05 18:27:29 +0000

    Added more colors to choose from

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "black", "violet", "magenta", "blue", "gray", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', () => {
+    const randomNumber = randomGen();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+});
+
+const randomGen = () => {
+    return Math.floor(Math.random() * colors.length)
+}
